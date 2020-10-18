2c38e57603a70862df2a65bf6341237c484c5801
fengxueem <fengxueem@gmail.com>
2020-10-18 12:06:01 +0800

    fix a bug: extra 0 was put in front of Oct.

--- js/index.js
+++ js/index.js
@@ -12,8 +12,8 @@ var getWindowHeight = function(){
 var getDiffDate = function(start, end) {
     var dateArr = [];
     while ((end.getTime() - start.getTime()) > 0) {
-        var year = start.getFullYear();            
-        var month = start.getMonth().toString().length === 1 ? "0" + (parseInt(start.getMonth().toString(),10) + 1) : (start.getMonth() + 1);
+        var year = start.getFullYear();
+        var month = start.getMonth() + 1;
         var day = start.getDate().toString().length === 1 ? "0" + start.getDate() : start.getDate();
         dateArr.push(year + "-" + month + "-" + day);
         start.setDate(start.getDate() + 1);
